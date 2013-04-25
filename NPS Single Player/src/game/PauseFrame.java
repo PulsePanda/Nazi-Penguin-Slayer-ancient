be15ccc9eaf1a96310c5e35814fa1aefd0edbfbf
Austin VanAlstyne <ajv857@gmail.com>
2013-04-25 11:42:03 -0500

    changed the buttons again on save
    changed the labels for the save back to jbuttons, and this time deleted
    everything, so to fix this, clearly revert it. now i need to figure out
    how i want to resize the buttons...

--- NPS Single Player/src/game/PauseFrame.java
+++ NPS Single Player/src/game/PauseFrame.java
@@ -19,72 +19,41 @@ import javax.swing.JLabel;
 
 public class PauseFrame extends JFrame {
 
-	// JButton resume, saveAndExit;
-	JLabel resume, saveAndExit;
+	JButton resume, saveAndExit;
+	// JLabel resume, saveAndExit;
 
 	JFrame f = this;
 
 	public PauseFrame() {
 		IO io = new IO();
 
-		// resume = new JButton("resume");
-		// // resume.setIcon(new
+		resume = new JButton("resume");
+		// resume.setIcon(new
 		// ImageIcon(io.getImage(FILES.resumeButtonImage)));
-		// resume.setBounds((Core.getWidth() / 4) - (250 / 2), 75, 250, 50);
-		// resume.setBorderPainted(false);
-		// // resume.setRolloverIcon(new ImageIcon(io
-		// // .getImage(FILES.resumeButtonOverlay)));
-		// resume.addActionListener(new ActionListener() {
-		// public void actionPerformed(ActionEvent arg0) {
-		// dispose();
-		// Core.pause();
-		// }
-		// });
-		//
-		// saveAndExit = new JButton("saveAndExit");
-		// // saveAndExit.setIcon(new ImageIcon(io
-		// // .getImage(FILES.saveAndExitButtonImage)));
-		// saveAndExit.setBounds((Core.getWidth() / 4) - (250 / 2), 175, 250,
-		// 50);
-		// saveAndExit.setBorderPainted(false);
-		// // saveAndExit.setRolloverIcon(new ImageIcon(io
-		// // .getImage(FILES.saveAndExitButtonOverlay)));
-		// saveAndExit.addActionListener(new ActionListener() {
-		// public void actionPerformed(ActionEvent arg0) {
-		// Core.save();
-		// Core.stop();
-		// }
-		// });
-
-		resume = new JLabel(new ImageIcon(io.getImage(FILES.resumeButtonImage)));
-		resume.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				try {
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+		resume.setBounds((Core.getWidth() / 4) - (250 / 2), 75, 250, 50);
+		resume.setBorderPainted(false);
+		// resume.setRolloverIcon(new ImageIcon(io
+		// .getImage(FILES.resumeButtonOverlay)));
+		resume.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				Core.pause();
 			}
 		});
-		int labelWidth = Core.getWidth() - 50, labelHeight = Core.getHeight() / 3;
-		resume.setBounds((Core.getWidth() / 4) - (labelWidth / 2),
-				(Core.getHeight() / 3) - (labelHeight / 2), labelWidth,
-				labelHeight);
 
-		saveAndExit = new JLabel(new ImageIcon(
-				io.getImage(FILES.saveAndExitButtonImage)));
-		saveAndExit.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				try {
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+		saveAndExit = new JButton("saveAndExit");
+		// saveAndExit.setIcon(new ImageIcon(io
+		// .getImage(FILES.saveAndExitButtonImage)));
+		saveAndExit.setBounds((Core.getWidth() / 4) - (250 / 2), 175, 250, 50);
+		saveAndExit.setBorderPainted(false);
+		// saveAndExit.setRolloverIcon(new ImageIcon(io
+		// .getImage(FILES.saveAndExitButtonOverlay)));
+		saveAndExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Core.save();
+				Core.stop();
 			}
 		});
-		saveAndExit.setBounds((Core.getWidth() / 4) - (labelWidth / 2),
-				(Core.getHeight() / 3) + (Core.getHeight() / 3)
-						- (labelHeight / 2), labelWidth, labelHeight);
 
 		setLayout(null);
 		setAlwaysOnTop(true);
