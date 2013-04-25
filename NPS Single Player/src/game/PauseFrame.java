ee4515d85fb02e79a3cf7a87607207a55eab6e25
Austin VanAlstyne <ajv857@gmail.com>
2013-04-25 11:21:06 -0500

    Revert "reverting this.... its a test"
    This reverts commit f507911d21f19696b27f10af42ffe2a639f9aca6.

--- NPS Single Player/src/game/PauseFrame.java
+++ NPS Single Player/src/game/PauseFrame.java
@@ -19,66 +19,66 @@ import javax.swing.JLabel;
 
 public class PauseFrame extends JFrame {
 
-	JButton resume, saveAndExit;
-//	JLabel resume, saveAndExit;
+//	JButton resume, saveAndExit;
+	JLabel resume, saveAndExit;
 	
 	JFrame f = this;
 
 	public PauseFrame() {
 		IO io = new IO();
 
-		resume = new JButton("resume");
-		// resume.setIcon(new ImageIcon(io.getImage(FILES.resumeButtonImage)));
-		resume.setBounds((Core.getWidth() / 4) - (250 / 2), 75, 250, 50);
-		resume.setBorderPainted(false);
-		// resume.setRolloverIcon(new ImageIcon(io
-		// .getImage(FILES.resumeButtonOverlay)));
-		resume.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				dispose();
-				Core.pause();
-			}
-		});
-
-		saveAndExit = new JButton("saveAndExit");
-		// saveAndExit.setIcon(new ImageIcon(io
-		// .getImage(FILES.saveAndExitButtonImage)));
-		saveAndExit.setBounds((Core.getWidth() / 4) - (250 / 2), 175, 250, 50);
-		saveAndExit.setBorderPainted(false);
-		// saveAndExit.setRolloverIcon(new ImageIcon(io
-		// .getImage(FILES.saveAndExitButtonOverlay)));
-		saveAndExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Core.save();
-				Core.stop();
-			}
-		});
-
-//		resume = new JLabel(new ImageIcon(
-//				io.getImage(FILES.resumeButtonImage)));
-//		resume.addMouseListener(new MouseAdapter() {
-//			public void mouseClicked(MouseEvent e) {
-//				try {
-//				} catch (Exception e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
+//		resume = new JButton("resume");
+//		// resume.setIcon(new ImageIcon(io.getImage(FILES.resumeButtonImage)));
+//		resume.setBounds((Core.getWidth() / 4) - (250 / 2), 75, 250, 50);
+//		resume.setBorderPainted(false);
+//		// resume.setRolloverIcon(new ImageIcon(io
+//		// .getImage(FILES.resumeButtonOverlay)));
+//		resume.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				dispose();
+//				Core.pause();
 //			}
 //		});
-//		resume.setBounds((Core.getWidth() / 4) - (250 / 2), 175, 250, 50);
-//		
-//		saveAndExit = new JLabel(new ImageIcon(
-//				io.getImage(FILES.saveAndExitButtonImage)));
-//		saveAndExit.addMouseListener(new MouseAdapter() {
-//			public void mouseClicked(MouseEvent e) {
-//				try {
-//				} catch (Exception e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
+//
+//		saveAndExit = new JButton("saveAndExit");
+//		// saveAndExit.setIcon(new ImageIcon(io
+//		// .getImage(FILES.saveAndExitButtonImage)));
+//		saveAndExit.setBounds((Core.getWidth() / 4) - (250 / 2), 175, 250, 50);
+//		saveAndExit.setBorderPainted(false);
+//		// saveAndExit.setRolloverIcon(new ImageIcon(io
+//		// .getImage(FILES.saveAndExitButtonOverlay)));
+//		saveAndExit.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				Core.save();
+//				Core.stop();
 //			}
 //		});
-//		saveAndExit.setBounds((Core.getWidth() / 4) - (250 / 2), 75, 250, 50);
+
+		resume = new JLabel(new ImageIcon(
+				io.getImage(FILES.resumeButtonImage)));
+		resume.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				try {
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		resume.setBounds((Core.getWidth() / 4) - (250 / 2), 175, 250, 50);
+		
+		saveAndExit = new JLabel(new ImageIcon(
+				io.getImage(FILES.saveAndExitButtonImage)));
+		saveAndExit.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				try {
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		saveAndExit.setBounds((Core.getWidth() / 4) - (250 / 2), 75, 250, 50);
 		
 		setLayout(null);
 		setAlwaysOnTop(true);
