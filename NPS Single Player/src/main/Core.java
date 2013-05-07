eeb6784da6e4f7ac4ee6db06ac6683ba5d9951b4
Austin VanAlstyne <ajv857@gmail.com>
2013-05-07 10:18:28 -0500

    made the generation of tiles more intuitive. made it so it will automatically do the rest based on whats above.

--- NPS Single Player/src/main/Core.java
+++ NPS Single Player/src/main/Core.java
@@ -6,48 +6,78 @@ import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 
 public class Core {
-	private static int tilew = 50, tileh = 30, day = 0;
+	private static int tilew = 50, tileh = 30, day = 0, barValue = 0;
 	public static Tile[][] tiles = new Tile[tilew][tileh];
 	public static ArrayList list = new ArrayList();
 	private static Core core;
 	private static Frame frame;
+	private static JProgressBar bar = new JProgressBar();
 	private static boolean running = true, paused = false;
 
 	public Core() {
-		initTiles();
-	}
-
-	public void initTiles() {
-		// test!!!
-		int barValue = 0;
-		Frame loadingFrame = new Frame("Loading World...", null, 300, 200);
-		loadingFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		loadingFrame.setLayout(null);
-
-		JProgressBar bar = new JProgressBar();
 		bar.setBounds(20, 50, 255, 25);
-		bar.setMaximum(tilew * tileh);
+		bar.setMaximum(tilew * tileh * 2);
 		bar.setMinimum(0);
 		bar.setValue(barValue);
 
-		loadingFrame.add(bar);
-		loadingFrame.setLocationRelativeTo(null);
+		createTiles();
+		initTiles();
+	}
 
+	public void createTiles() {
 		for (int w = 0; w < tilew; w++) {
 			for (int h = 0; h < tileh; h++) {
 				barValue++;
 				bar.setValue(barValue);
-				if (h == tileh / 2)
-					tiles[w][h] = new Tile(1);
-				else if (h > tileh / 2)
-					tiles[w][h] = new Tile(2);
-				else
-					tiles[w][h] = new Tile(0);
+				tiles[w][h] = new Tile(0);
 				list.add(tiles[w][h]);
 			}
 		}
+	}
 
-		// loadingFrame.delete();
+	public void initTiles() {
+		// test!!!
+		Frame loadingFrame = new Frame("Loading World...", null, 300, 200);
+		loadingFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		loadingFrame.setLayout(null);
+
+		loadingFrame.add(bar);
+		loadingFrame.setLocationRelativeTo(null);
+
+		// for (int w = 0; w < tilew; w++) {
+		// for (int h = 0; h < tileh; h++) {
+		// barValue++;
+		// bar.setValue(barValue);
+		// if (h == tileh / 2)
+		// tiles[w][h] = new Tile(1);
+		// else if (h > tileh / 2)
+		// tiles[w][h] = new Tile(2);
+		// else
+		// tiles[w][h] = new Tile(0);
+		// list.add(tiles[w][h]);
+		// }
+		// }
+		for (int w = 0; w < tilew; w++) {
+			for (int h = 0; h < tileh; h++) {
+				barValue++;
+				bar.setValue(barValue);
+				if (h == tileh / 2) // set the grass lvl at half way
+					tiles[w][h] = new Tile(1);
+				if (tiles[w][getOneLessThan(h)].getID() == 1) // if tile above
+																// is grass
+					tiles[w][h] = new Tile(2);
+				if (tiles[w][getOneLessThan(h)].getID() == 2) // if tile above
+																// is dirt
+					tiles[w][h] = new Tile(2);
+				if (h == 25)
+					tiles[w][h] = new Tile(3);
+				if (tiles[w][getOneLessThan(h)].getID() == 3) // if tile above
+																// is stone
+					tiles[w][h] = new Tile(3);
+			}
+		}
+
+		loadingFrame.delete();
 	}
 
 	public int getOneLessThan(int i) {
