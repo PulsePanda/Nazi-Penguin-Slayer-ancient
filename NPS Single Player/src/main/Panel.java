63cbfc2eecdeea2d4a8dcd260d8816df247b0fa2
Austin VanAlstyne <ajv857@gmail.com>
2013-05-08 13:52:36 -0500

    made the way the tiles are placed adjust their X and Y value every single time. offset now affects it, so the 'getBounds()' will work later

--- NPS Single Player/src/main/Panel.java
+++ NPS Single Player/src/main/Panel.java
@@ -20,11 +20,12 @@ public class Panel extends JPanel {
 		for (int i = 0; i < Core.getTileArrayWidth(); i++) {
 			for (int a = 0; a < Core.getTileArrayHeight(); a++) {
 				Tile tile = tiles[i][a];
-				int x = i * tile.w, y = a * tile.h;
+				int x = i * tile.w + Core.getXOff(), y = a * tile.h
+						+ Core.getYOff();
 				tile.setX(x);
 				tile.setY(y);
-				g.drawImage(tile.getImage(), 0 + (i * tile.w) + Core.getXOff(),
-						0 + (a * tile.h) + Core.getYOff(), tile.w, tile.h, null);
+				g.drawImage(tile.getImage(), tile.getX(), tile.getY(), tile.w,
+						tile.h, null);
 			}
 		}
 	}
