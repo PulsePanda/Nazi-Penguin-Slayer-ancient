6d893cd849739f0b5e3fc6db638f529492fbc623
Austin VanAlstyne <ajv857@gmail.com>
2013-05-28 13:49:28 -0500

    roll these back

--- NPS Main/src/main/Login.java
+++ NPS Main/src/main/Login.java
@@ -22,6 +22,7 @@ import java.util.Properties;
 import javax.swing.*;
 
 import panels.MainScreenPanel;
+import panels.NewsPanel;
 
 public class Login extends Start {
 
@@ -31,103 +32,95 @@ public class Login extends Start {
 	static JButton cancel, login;
 	public static boolean serverOnline = true;
 	static boolean loggedIn = false;
-	static final int WIDTH = 200, HEIGHT = 225;
+	// static final int WIDTH = 200, HEIGHT = 225;
+	static final int width = 850, height = 500;
 	public static String username, password;
 	public static boolean loggedin = false;
 	public static Properties userProperties = new Properties();
-	private static JEditorPane display;
+	private static NewsPanel newsPanel;
 
-	public static void createFrame() {
-		if (!serverOnline) {
-			Connection c = new Connection("nazipenguinslayer.no-ip.org",
-					"6987", null, null);
-			JOptionPane.showMessageDialog(null,
-					"Server offline. Please try again later or check the FAQ.");
-			System.exit(0);
-		}
-
-		f = new JFrame("Login");
-		user = new JTextField();
-		pass = new JPasswordField();
-		cancel = new JButton();
-		login = new JButton();
-
-		user.setBounds(5, 10, WIDTH - 15, 25);
-		user.setText("Username");
-		user.addFocusListener(new FocusListener() {
-			public void focusGained(FocusEvent arg0) {
-				user.setText("");
-			}
-
-			public void focusLost(FocusEvent arg0) {
-			}
-		});
-
-		pass.setBounds(5, 40, WIDTH - 15, 25);
-		pass.setText("Password");
-		pass.addFocusListener(new FocusListener() {
-			public void focusGained(FocusEvent arg0) {
-				pass.setText("");
-			}
-
-			public void focusLost(FocusEvent arg0) {
-			}
-		});
-
-		login.setBounds(5, 80, WIDTH - 15, 50);
-		login.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				login(user.getText(), pass.getText());
-			}
-		});
-		login.setText("add image, login");
-
-		cancel.setBounds(5, 135, WIDTH - 15, 50);
-		cancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				System.exit(0);
-			}
-		});
-		cancel.setText("add image, cancel");
-
-		f.setResizable(false);
-		f.setAlwaysOnTop(true);
-		f.setSize(WIDTH, HEIGHT);
-		f.setLocationRelativeTo(null);
-		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		f.setLayout(null);
-		f.add(user);
-		f.add(pass);
-		f.add(cancel);
-		f.add(login);
-		f.setFocusable(true);
-		f.setVisible(true);
-
-		// make frame show on top of other windows
-		f.setAlwaysOnTop(true);
-		f.toFront();
-		f.requestFocus();
-		f.setAlwaysOnTop(false);
-	}
+	// public static void createFrame() {
+	// if (!serverOnline) {
+	// Connection c = new Connection("nazipenguinslayer.no-ip.org",
+	// "6987", null, null);
+	// JOptionPane.showMessageDialog(null,
+	// "Server offline. Please try again later or check the FAQ.");
+	// System.exit(0);
+	// }
+	//
+	// f = new JFrame("Login");
+	// user = new JTextField();
+	// pass = new JPasswordField();
+	// cancel = new JButton();
+	// login = new JButton();
+	//
+	// user.setBounds(5, 10, WIDTH - 15, 25);
+	// user.setText("Username");
+	// user.addFocusListener(new FocusListener() {
+	// public void focusGained(FocusEvent arg0) {
+	// user.setText("");
+	// }
+	//
+	// public void focusLost(FocusEvent arg0) {
+	// }
+	// });
+	//
+	// pass.setBounds(5, 40, WIDTH - 15, 25);
+	// pass.setText("Password");
+	// pass.addFocusListener(new FocusListener() {
+	// public void focusGained(FocusEvent arg0) {
+	// pass.setText("");
+	// }
+	//
+	// public void focusLost(FocusEvent arg0) {
+	// }
+	// });
+	//
+	// login.setBounds(5, 80, WIDTH - 15, 50);
+	// login.addActionListener(new ActionListener() {
+	// public void actionPerformed(ActionEvent e) {
+	// login(user.getText(), pass.getText());
+	// }
+	// });
+	// login.setText("add image, login");
+	//
+	// cancel.setBounds(5, 135, WIDTH - 15, 50);
+	// cancel.addActionListener(new ActionListener() {
+	// public void actionPerformed(ActionEvent arg0) {
+	// System.exit(0);
+	// }
+	// });
+	// cancel.setText("add image, cancel");
+	//
+	// f.setResizable(false);
+	// f.setAlwaysOnTop(true);
+	// f.setSize(WIDTH, HEIGHT);
+	// f.setLocationRelativeTo(null);
+	// f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	// f.setLayout(null);
+	// f.add(user);
+	// f.add(pass);
+	// f.add(cancel);
+	// f.add(login);
+	// f.setFocusable(true);
+	// f.setVisible(true);
+	//
+	// // make frame show on top of other windows
+	// f.setAlwaysOnTop(true);
+	// f.toFront();
+	// f.requestFocus();
+	// f.setAlwaysOnTop(false);
+	// }
 
 	public static void createFrame2() {
 		f = new JFrame("Nazi Penguin Slayer   Version: " + version);
-		f.setSize(850, 500);
+		f.setSize(width, height);
 		f.setResizable(true);
 		f.setLocationRelativeTo(null);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		display = new JEditorPane();
-		display.setEditable(false);
-		try {
-			display.setPage("http://npslayer.tumblr.com");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		user = new JTextField();
-
-		f.add(new JScrollPane(display), BorderLayout.CENTER);
+		newsPanel = new NewsPanel(width, height);
+		f.add(newsPanel);
 		f.setVisible(true);
 	}
 
