b7b6b7f8ba848a63084f692aaf05f7f9254883b9
Austin VanAlstyne <ajv857@gmail.com>
2013-09-04 20:45:05 -0500

    finishing clean up

--- NPS Single Player/src/main/FILES.java
+++ NPS Single Player/src/main/FILES.java
@@ -6,24 +6,24 @@ import java.io.FileOutputStream;
 import java.util.Properties;
 
 public class FILES {
-//	public static final String dirtBlock = "../graphic/Dirt Block.png";
-//	public static final String stoneBlock = "../graphic/Stone Block.png";
-//	public static final String grassBlock = "../graphic/Grass Block.png";
-//	public static final String backgroundImage = "../graphic/Game Background.png";
-//	public static final String playerImage = "../graphic/Player.png";
-//	public static final String ingameMenuBackground = "../graphic/Ingame Menu Background.png";
-//	public static final String properties = "properties.properties";
+	public static final String dirtBlock = "../graphic/Dirt Block.png";
+	public static final String stoneBlock = "../graphic/Stone Block.png";
+	public static final String grassBlock = "../graphic/Grass Block.png";
+	public static final String backgroundImage = "../graphic/Game Background.png";
+	public static final String playerImage = "../graphic/Player.png";
+	public static final String ingameMenuBackground = "../graphic/Ingame Menu Background.png";
+	public static final String properties = "properties.properties";
 
-	 public static final String dirtBlock = "C:\\dirt.png";
-	 public static final String stoneBlock = "C:\\stone.png";
-	 public static final String grassBlock = "C:\\grass.png";
-	 public static final String backgroundImage = "C:\\back.png";
-	 public static final String playerImage = "C:\\player.png";
-	 public static final String ingameMenuBackground = "C:\\imb.png";
+	// public static final String dirtBlock = "C:\\dirt.png";
+	// public static final String stoneBlock = "C:\\stone.png";
+	// public static final String grassBlock = "C:\\grass.png";
+	// public static final String backgroundImage = "C:\\back.png";
+	// public static final String playerImage = "C:\\player.png";
+	// public static final String ingameMenuBackground = "C:\\imb.png";
 	// public static final String properties =
 	// "A:\\Google Drive\\AVTECH\\NPS\\Files\\bin\\properties.properties";
-	 public static final String properties =
-	 "C:\\Users\\Austin\\Google Drive\\AVTECH\\NPS\\Files\\bin\\properties.properties";
+	// public static final String properties =
+	// "C:\\Users\\Austin\\Google Drive\\AVTECH\\NPS\\Files\\bin\\properties.properties";
 
 	public static final String version = "0.0.1";
 
