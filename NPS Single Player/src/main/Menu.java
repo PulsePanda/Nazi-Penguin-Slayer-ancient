5eb199970eebdeb5abd5c76d30125d5efbede03d
Austin VanAlstyne <ajv857@gmail.com>
2013-10-15 17:21:47 -0500

    made all ingame frames extend Menu

---
+++ NPS Single Player/src/main/Menu.java
@@ -0,0 +1,83 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JPanel;
+
+public class Menu extends JPanel {
+	protected int w = 400, h = 300;
+	protected Core core = Core.getCore();
+	protected static final IO io = new IO();
+	protected Point point = new Point();
+	protected String title = "";
+
+	public Menu(String t) {
+		title = t;
+		init();
+	}
+
+	public Menu(String t, int w, int h) {
+		title = t;
+		this.w = w;
+		this.h = h;
+		init();
+	}
+
+	public void init() {
+		setSize(w, h);
+		setLayout(null);
+		setBackground(new Color(0, 0, 0, 0));
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					remove();
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+			}
+		});
+
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				requestFocus();
+			}
+		});
+
+		this.setFocusable(true);
+
+		setVisible(false);
+	}
+
+	public void remove() {
+		setVisible(false);
+		core.getFrame().requestFocus();
+	}
+
+	public void setMoveable() {
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				point.x = e.getX();
+				point.y = e.getY();
+			}
+		});
+		addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				Point p = getLocation();
+				setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
+			}
+		});
+	}
+}
--- NPS Single Player/src/main/PauseMenu.java
+++ NPS Single Player/src/main/PauseMenu.java
@@ -13,76 +13,14 @@ import java.awt.event.MouseMotionAdapter;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class PauseMenu extends JPanel {
-
-	private int w = 400, h = 300;
-	private Core core = Core.getCore();
-	private static final IO io = new IO();
-	private Point point = new Point();
-	private String title = "";
+public class PauseMenu extends Menu {
 
 	public PauseMenu(String t) {
-		title = t;
-		init();
+		super(t);
 	}
 
 	public PauseMenu(String t, int w, int h) {
-		title = t;
-		this.w = w;
-		this.h = h;
-		init();
-	}
-
-	public void init() {
-		setSize(w, h);
-		setLayout(null);
-		setBackground(new Color(0, 0, 0, 0));
-		addKeyListener(new KeyListener() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-					remove();
-				}
-			}
-
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-			}
-
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-
-		addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				requestFocus();
-			}
-		});
-
-		this.setFocusable(true);
-
-		setVisible(false);
-	}
-
-	public void remove() {
-		setVisible(false);
-		core.getFrame().requestFocus();
-	}
-
-	public void setMoveable() {
-		addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				point.x = e.getX();
-				point.y = e.getY();
-			}
-		});
-		addMouseMotionListener(new MouseMotionAdapter() {
-			public void mouseDragged(MouseEvent e) {
-				Point p = getLocation();
-				setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
-			}
-		});
+		super(t, w, h);
 	}
 
 	public void paint(Graphics g) {
