b98b661e5652bfe6a66a37f93854629b9cc4a8dc
Austin VanAlstyne <ajv857@gmail.com>
2013-10-24 11:58:58 -0500

    desided against this shit

---
+++ NPS Single Player version 1.1/src/avtech/software/nps/components/Game.java
@@ -0,0 +1,15 @@
+package avtech.software.nps.components;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements Runnable {
+
+	private static boolean running = false;
+
+	public void run() {
+		while (running) {
+
+		}
+	}
+
+}
